894ae3ac836a9b385fca3fa2b651885c955aa441
이용재 <dididy@knou.ac.kr>
2019-07-02 23:23:47 +0900

    Some part complete

--- server.py
+++ server.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def get_index():
     return render_template('index.html')
 
-@app.route("/keyboard")
-def keyboard():
-    return render_template('keyboard.html')
+@app.route("/flag")
+def get_flag():
+    return render_template('flag.html')
 
 app.run(host='0.0.0.0')
 
